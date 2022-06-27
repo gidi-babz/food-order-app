3616ea259e3d6c5966c5cd7243e5f342fb4708e1
Gidi Babz <tunex4ind@gmail.com>
2022-06-27 12:34:49 +0100

    more commits

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -5,11 +5,18 @@ import MealItem from './MealItems/MealItem';
 
 const AvailableMeals = () => {
   const [meal, setMeal] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
+
   useEffect(() => {
     async function fetchData() {
       const res = await fetch(
         'https://react-fetch-3ecf4-default-rtdb.firebaseio.com/meals%20.json'
       );
+
+      if (!res.ok) {
+        throw new Error('Something went wrong!');
+      }
       const data = await res.json();
       console.log(data);
 
@@ -18,20 +25,38 @@ const AvailableMeals = () => {
       for (const mealKey in data) {
         mealsList.push({
           ...data[mealKey],
-          key: mealKey,
+          id: mealKey,
         });
       }
 
       setMeal(mealsList);
+      setIsLoading(false);
     }
-    fetchData();
+    fetchData().catch(error => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
 
-  console.log(meal);
+  if (isLoading) {
+    return (
+      <section className={classes.mealLoading}>
+        <p>loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.mealError}>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
 
   const mealsListItems = meal.map(meal => (
     <MealItem
-      key={meal.key}
+      key={meal.id}
       id={meal.id}
       name={meal.name}
       description={meal.description}
